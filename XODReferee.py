fd86a8bd62ddf8639799e02d1caf70ac78eddf9a
drisbee <mail2dre@gmail.com>
2014-12-28 21:38:57 +0100

    Final commit

--- XODReferee.py
+++ XODReferee.py
@@ -2,27 +2,46 @@ __author__ = 'Andres'
 
 
 def checkio(game_result):
+    three = [(0,0)]
     for i in range(3):
         if game_result[i] == 'XXX':
             return "X"
-        elif game_result[i] == 'OOO':
+            break
+        if game_result[i] == 'OOO':
             return "O"
-        else:
-            for row in range(3):
-                if game_result[0][row] + game_result[1][row] + game_result[2][row] == "XXX":
-                    return "X"
-                elif game_result[0][row] + game_result[1][row] + game_result[2][row] == "XXX":
-                    return "O"
-                else:
-                    if (game_result[0][2] + game_result[1][1] + game_result[2][0] == "XXX") or (
-                                        game_result[0][0] + game_result[1][1] + game_result[2][2] == "XXX"):
-                        return "X"
-                    elif (game_result[0][2] + game_result[1][1] + game_result[2][0] == "OOO") or (
-                                        game_result[0][0] + game_result[1][1] + game_result[2][2] == "OOO"):
-                        return "O"
-                    else:
-                        return "D"
+            break
+        if (game_result[0][0] + game_result[1][0] + game_result[2][0] == "XXX"
+            ) or (game_result[0][1] + game_result[1][1] + game_result[2][1] == "XXX"
+            ) or (game_result[0][2] + game_result[1][2] + game_result[2][2] == "XXX"
+            ) or (game_result[0][2] + game_result[1][1] + game_result[2][0] == "XXX"
+            ) or (game_result[0][0] + game_result[1][1] + game_result[2][2] == "XXX"):
+            return "X"
+            break
+        if (game_result[0][0] + game_result[1][0] + game_result[2][0] == "OOO"
+            ) or (game_result[0][1] + game_result[1][1] + game_result[2][1] == "OOO"
+            ) or (game_result[0][2] + game_result[1][2] + game_result[2][2] == "OOO"
+            ) or (game_result[0][2] + game_result[1][1] + game_result[2][0] == "OOO"
+            ) or (game_result[0][0] + game_result[1][1] + game_result[2][2] == "OOO"):
+            return "O"
+            break
+    else:
+        return "D"
 
 
-
-print checkio(([u'O.X', u'X.X', u'XOO']))
+print checkio([u"OO.",u"XXX",u"XOX"])
+print checkio([
+        u"X.O",
+        u"XX.",
+        u"XOO"])== "X", "Xs wins"
+print checkio([
+        u"OO.",
+        u"XOX",
+        u"XOX"]) == "O", "Os wins"
+print checkio([
+        u"OOX",
+        u"XXO",
+        u"OXX"]) == "D", "Draw"
+print checkio([
+        u"O.X",
+        u"XX.",
+        u"XOO"]) == "X", "Xs wins again"
